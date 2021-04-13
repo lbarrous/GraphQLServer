0d1762b644f1400eef8b0f364553801938279262
Alvaro Perez Garcia <apgarcia@MacBook-Pro-de-Alvaro.local>
2021-04-13 14:58:10 +0200

    Create apollo server with schemas, resolvers and datasources

---
+++ src/schema.js
@@ -0,0 +1,23 @@
+const { gql } = require('apollo-server');
+const typeDefs = gql`
+  type Muscle {
+    id: ID!
+    name: String
+    description: String
+    videos: [Video]
+  }
+  type Video {
+    id: ID!
+    name: String
+    description: String
+    videoLink: String
+    imageLink: String
+  }
+  type Query {
+    muscles: [Muscle]!
+    muscle(id: ID!): Muscle
+    videos: [Video]!
+    video(id: ID!): Video 
+  }
+`;
+module.exports = typeDefs;
