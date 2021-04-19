97f6c9680d694ad57604bbdfe3abe0a27ee5bb99
Alvaro Perez Garcia <apgarcia@MacBook-Pro-de-Alvaro.local>
2021-04-19 17:23:35 +0200

    Graphql API finished

---
+++ src/graphql.js
@@ -0,0 +1,30 @@
+const { ApolloServer } = require('apollo-server-lambda');
+const typeDefs = require('./schema');
+const muscleDatabase = require("./datasources/muscle");
+const resolvers = require('./resolvers');
+const knexConfig = {
+  client: 'mysql',
+  connection: {
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_SCHEMA,
+    port: '3306'
+  }
+};
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({ 
+    muscleDatabase: new muscleDatabase(knexConfig)
+  }),
+  playground: {
+    endpoint: "/dev/graphql"
+  }
+});
+exports.graphqlHandler = server.createHandler({
+  cors: {
+    origin: '*',
+    credentials: false,
+  },
+});
